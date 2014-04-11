bc82a7759ae94eabc08fce0d7bc9d5057541d672
3yi <lanou@lanoutekiMac-mini.local>
2014-04-11 14:19:41 +0800

    Initial Commit

---
+++ 国际化/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  国际化
+//
+//  Created by 3yi on 14-4-11.
+//  Copyright (c) 2014年 3yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 国际化/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  国际化
+//
+//  Created by 3yi on 14-4-11.
+//  Copyright (c) 2014年 3yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
